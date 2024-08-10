baae2ffa99655cef395e8574720c2ebdac48a827
zafar-m3n <thezafar.m3n@gmail.com>
2024-08-10 13:07:51 +0530

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Student;
 use App\Models\Lecturer;
 use App\Models\Parents;
+use App\Models\Course;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,12 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create Users
         User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('password'),
             'role' => 'admin',
         ]);
+
         $studentUser = User::factory()->create([
             'name' => 'Student User',
             'email' => 'student@gmail.com',
@@ -50,6 +53,27 @@ class DatabaseSeeder extends Seeder
         ]);
         Parents::create([
             'user_id' => $parentUser->id,
+        ]);
+
+        Course::create([
+            'course_code' => 'ACC101',
+            'course_name' => 'Accounting',
+            'description' => 'Introduction to Accounting',
+            'stream' => 'Commerce',
+        ]);
+
+        Course::create([
+            'course_code' => 'BUS102',
+            'course_name' => 'Business Studies',
+            'description' => 'Fundamentals of Business Studies',
+            'stream' => 'Commerce',
+        ]);
+
+        Course::create([
+            'course_code' => 'ECO103',
+            'course_name' => 'Economics',
+            'description' => 'Principles of Economics',
+            'stream' => 'Commerce',
         ]);
     }
 }
